5e3d72feccfa36887397ef3428127a0a54589a20
Matheus Fonseca <teus.fonseca@hotmail.com>
2023-11-16 16:10:53 -0300

    Criação da tabela de produtos

---
+++ prisma/migrations/20231116191042_create_products_table/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "products" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
+    "price" DECIMAL(65,30) NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "products_id_key" ON "products"("id");
