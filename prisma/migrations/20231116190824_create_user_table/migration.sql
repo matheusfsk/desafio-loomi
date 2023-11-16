546107586b1fdc1f870501dcee4220edcf105b8b
Matheus Fonseca <teus.fonseca@hotmail.com>
2023-11-16 16:09:15 -0300

    Criação da tabela de usuarios

---
+++ prisma/migrations/20231116190824_create_user_table/migration.sql
@@ -0,0 +1,24 @@
+-- CreateEnum
+CREATE TYPE "UserType" AS ENUM ('admin', 'customer');
+
+-- CreateEnum
+CREATE TYPE "OrderStatus" AS ENUM ('received', 'in_preparation', 'dispatched', 'delivered');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "user_type" "UserType" NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_id_key" ON "users"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
