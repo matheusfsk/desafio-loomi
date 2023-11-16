ffefb62c3043a3e95259bcdc1ebaaae94024ba25
Matheus Fonseca <teus.fonseca@hotmail.com>
2023-11-16 16:11:31 -0300

    Criação da tabela de pedidos

---
+++ prisma/migrations/20231116191121_create_orders_table/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "orders" (
+    "id" SERIAL NOT NULL,
+    "customer_id" INTEGER NOT NULL,
+    "order_status" "OrderStatus" NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "orders_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "orders_id_key" ON "orders"("id");
+
+-- AddForeignKey
+ALTER TABLE "orders" ADD CONSTRAINT "orders_customer_id_fkey" FOREIGN KEY ("customer_id") REFERENCES "customers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
