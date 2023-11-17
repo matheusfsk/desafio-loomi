262f006b8c778a550f29f8b13468f7cdbcfb3934
Matheus Fonseca <teus.fonseca@hotmail.com>
2023-11-17 15:07:08 -0300

    Adicionando nova tabela de categorias e inserindo coluna de categorias em produtos

---
+++ prisma/migrations/20231117180547_create_table_category/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - Added the required column `category_id` to the `products` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "products" ADD COLUMN     "category_id" INTEGER NOT NULL;
+
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" SERIAL NOT NULL,
+    "category" TEXT NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "categories_id_key" ON "categories"("id");
+
+-- AddForeignKey
+ALTER TABLE "products" ADD CONSTRAINT "products_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
