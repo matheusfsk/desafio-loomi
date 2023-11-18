40b0c38a9d9667999170e8a11ec01420b4a3bd8c
Matheus Fonseca <teus.fonseca@hotmail.com>
2023-11-18 10:30:57 -0300

    Inserido a função de atualizar produto

---
+++ src/controllers/Product/UpdateProductController.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from "express";
+import { prisma } from "../../database/prismaClient";
+
+export class UpdateProductController {
+  async handle(req: Request, res: Response) {
+    const { name, description, price, amount, category_id } = req.body;
+    const { id } = req.params;
+
+    try {
+      const productExists = await prisma.product.findFirst({
+        where: { id: Number(id) },
+      });
+
+      if (!productExists) {
+        return res.status(404).json({ mensagem: "Product not found." });
+      }
+
+      const categoryExists = await prisma.category.findFirst({
+        where: { id: Number(category_id) },
+      });
+
+      if (!categoryExists) {
+        return res.status(404).json({ mensagem: "Category not found." });
+      }
+
+      const updatedProduct = await prisma.product.update({
+        where: { id: Number(id) },
+        data: {
+          name,
+          description,
+          price,
+          amount,
+          category_id,
+        },
+      });
+
+      return res.status(201).json(updatedProduct);
+    } catch (error) {
+      return res.json({ message: "Internal server error" });
+    }
+  }
+}
+export default UpdateProductController;
