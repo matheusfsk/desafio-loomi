a28fa148bfbcfebca8469fb88082604579f35129
Matheus Fonseca <teus.fonseca@hotmail.com>
2023-11-18 10:12:51 -0300

    Inserida função de deletar produto

---
+++ src/controllers/Product/DeleteProductController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { prisma } from "../../database/prismaClient";
+
+export class DeleteProductController {
+  async handle(req: Request, res: Response) {
+    const { id } = req.params;
+
+    try {
+      const productExists = await prisma.product.findFirst({
+        where: { id: Number(id) },
+      });
+
+      if (!productExists) {
+        return res.status(404).json({ mensagem: "Product not found." });
+      }
+
+      await prisma.product.delete({
+        where: { id: Number(id) },
+      });
+      return res.status(204).send();
+    } catch (error) {
+      return res.json({ message: "Internal server error" });
+    }
+  }
+}
+export default DeleteProductController;
